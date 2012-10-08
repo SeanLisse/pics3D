3299b9c0cf81bd33f59c086fcf288c4a2726c061
Sean Lisse <sean.lisse@gmail.com>
2012-10-08 12:08:02 -0400

    Minor cleanup and fix setdebuglevel.

--- Utilities.py
+++ Utilities.py
@@ -22,14 +22,16 @@ debug_levels=enum('NO_DEBUG','BASIC_DEBUG','DETAILED_DEBUG')
 #debug_level=debug_levels.BASIC_DEBUG
 debug_level=debug_levels.DETAILED_DEBUG
 
-def debugprint(info, level=1):
+def debugprint(info, level = debug_levels.BASIC_DEBUG):
     ''' Prints info if level is less than or equal to the current debug_level '''
+    global debug_level
     if(level <= debug_level): 
         print(info)
         
 def setdebuglevel(level):
     ''' Set the current debug_level '''
+    global debug_level
     
     # Seems to be broken, so this is currently a no-op
-    # debug_level = level
+    debug_level = level
     
