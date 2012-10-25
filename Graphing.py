5752a58cdb1ea91655165f089a4697386f768d74
Sean Lisse <sean.lisse@gmail.com>
2012-10-25 13:25:18 -0400

    Added an experimental ability to draw columns underneath each fiducial point.

--- Graphing.py
+++ Graphing.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from Fiducials import COORDS
 
 DEFAULT_COLOR=[0.5,0.5,0.75]
+COLUMN_START=-50
 
 # Figure and Axes objects to contain our plots
 fig = plt.figure()
@@ -17,8 +18,9 @@ def default_color(fiducial):
     ''' Default coloration function. Clone this and alter it to customize your coloration when graphing. '''
     return DEFAULT_COLOR
 
-def show_graph():
+def show_graph(graphname="Fiducials"):
     ''' Display the graph.  Call this after adding all scatterpoints to it. '''
+    plt.title(graphname)
     plt.show()
 
 def add_scatterpoint_to_graph(name, x, y, z, newcolor="black"):
@@ -37,6 +39,12 @@ def add_fiducials_to_graph(fid_list, color_fn = default_color):
         fid = fid_list[key]
         add_scatterpoint_to_graph(fid.name, fid.coords[COORDS.X], fid.coords[COORDS.Y], fid.coords[COORDS.Z], color_fn(fid))
         
+def add_columns_to_graph(fid_list, color_fn = default_color):
+    ''' Add all fiducials in the dictionary fid_list to the graph as columns from Z=-50.  Color code using the function color_fn which takes a fiducial as an argument. '''
+    for key in fid_list.iterkeys():
+        fid = fid_list[key]
+        add_line_to_graph(fid.coords, [fid.coords[COORDS.X], fid.coords[COORDS.Y], COLUMN_START], color_fn(fid))
+        
 def add_legend_to_graph(minlabel, maxlabel, mincolor, maxcolor):
     ''' Create a legend for the graph, using matched tuples of labels and colors (e.g. labels[0] gets colors[0]'''
        
@@ -44,7 +52,7 @@ def add_legend_to_graph(minlabel, maxlabel, mincolor, maxcolor):
     minline = plt.Line2D(range(10), range(10), linestyle='-', label=minlabel, color=mincolor)
     maxline = plt.Line2D(range(10), range(10), linestyle='-', label=maxlabel, color=maxcolor)
     
-    ax.legend([minline,maxline], [minlabel,maxlabel], numpoints=2)
+    ax.legend([minline,maxline], [minlabel,maxlabel], numpoints=2, loc="lower right")
     
 def set_graph_boundaries(min_x, max_x, min_y, max_y, min_z, max_z):
     
