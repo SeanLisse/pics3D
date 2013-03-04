eca0a15d277a50692452e04dfeb91f6e84ce7701
Sean Lisse <sean.lisse@gmail.com>
2013-03-04 16:52:08 -0500

    Code cleanup and add 2D graph creation function.

--- PelvicPoints.py
+++ PelvicPoints.py
@@ -7,8 +7,8 @@ from Utilities import setdebuglevel, debug_levels, debugprint
 
 # My custom domain imports
 from VaginalProperties import VaginalDisplay
-from Graphing import add_fiducials_to_graph, add_line_to_graph, add_scatterpoint_to_graph
-from Graphing import add_legend_to_graph, set_graph_boundaries, show_all_graphs, PelvicGraph
+from Graphing import add_fiducials_to_graph3D, add_line_to_graph3D, add_scatterpoint_to_graph3D
+from Graphing import add_legend_to_graph3D, set_graph_boundaries3D, show_all_graphs, PelvicGraph3D
 from GraphColoring import calibrate_colorization_strategy_fn, fraction_color
 
 # Constants
@@ -20,32 +20,32 @@ PAD_GRAPH=False
 def create_pelvic_points_graph(graph, vagdisplay, graphname):
 
     if (graph == None):
-        graph = PelvicGraph(graphname)
+        graph = PelvicGraph3D(graphname)
     
     fid_points = vagdisplay._fiducial_points
     
     [color_fn, minmax_distances] = calibrate_colorization_strategy_fn(vagdisplay)
     
-    add_fiducials_to_graph(graph, vagdisplay, color_fn)
+    add_fiducials_to_graph3D(graph, vagdisplay, color_fn)
     
     PS_coords = vagdisplay._Pubic_Symphysis.coords
     L_IS_coords = vagdisplay._Left_IS.coords
     R_IS_coords = vagdisplay._Right_IS.coords
     
     # Display the P_IS lines on the graph as well
-    add_line_to_graph(graph, PS_coords, L_IS_coords , "black")
-    add_line_to_graph(graph, PS_coords, R_IS_coords, "black")
+    add_line_to_graph3D(graph, PS_coords, L_IS_coords , "black")
+    add_line_to_graph3D(graph, PS_coords, R_IS_coords, "black")
     
     IIS_coords = vagdisplay._IIS.coords
-    add_scatterpoint_to_graph(graph, INTER_ISCHIAL_SPINE_NAME, IIS_coords[COORDS.X], IIS_coords[COORDS.Y], IIS_coords[COORDS.Z], "black")
+    add_scatterpoint_to_graph3D(graph, INTER_ISCHIAL_SPINE_NAME, IIS_coords[COORDS.X], IIS_coords[COORDS.Y], IIS_coords[COORDS.Z], "black")
     
     if(fid_points.has_key(SC_JOINT_NAME)):
-        add_line_to_graph(graph, PS_coords, fid_points[SC_JOINT_NAME].coords, "black")
+        add_line_to_graph3D(graph, PS_coords, fid_points[SC_JOINT_NAME].coords, "black")
     
     if PAD_GRAPH:
         # Pad the graph to keep all graphs at equal scale for comparison
         GRAPH_PADDING=1
-        set_graph_boundaries(graph,
+        set_graph_boundaries3D(graph,
                              PS_coords[COORDS.X] + GRAPH_PADDING, 
                              PS_coords[COORDS.X] - GRAPH_PADDING,
                              PS_coords[COORDS.Y] + GRAPH_PADDING, 
@@ -54,7 +54,7 @@ def create_pelvic_points_graph(graph, vagdisplay, graphname):
                              PS_coords[COORDS.Z] - GRAPH_PADDING/3)
         
     # Add a legend
-    #    add_legend_to_graph(graph,
+    #    add_legend_to_graph3D(graph,
 #                        "Minimum Distance: " + str(round(minmax_distances[0],1)) + "mm", 
 #                        "Maximum Distance: " + str(round(minmax_distances[1],1)) + "mm",
 #                        fraction_color(minmax_distances[0], minmax_distances[0], minmax_distances[1]), 
