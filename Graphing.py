6bfdb87d7fda089e3848555bd30fe9027b5a6763
Sean Lisse <sean.lisse@gmail.com>
2013-03-04 16:50:41 -0500

    Code cleanup and add 2D graph creation function.

--- Graphing.py
+++ Graphing.py
@@ -9,10 +9,18 @@ import numpy as np
 # Default color for pelvic points
 DEFAULT_COLOR=[0.5,0.5,0.75]
 
-# Z coordinate for drawing columns to points (this defines the "base" of the column)
-COLUMN_START=-50
+class PelvicGraph2D(object):
+    def __init__(self, name="Fiducials", x_axis_name='X', y_axis_name='Y'):
+        # Figure and Axes objects to contain our plots
+        self._fig = plt.figure()
+        self._ax = self._fig.add_subplot('111')
+        
+        self._ax.set_xlabel(x_axis_name)
+        self._ax.set_ylabel(y_axis_name)
+        
+        plt.title(name)
 
-class PelvicGraph(object):
+class PelvicGraph3D(object):
     def __init__(self, name="Fiducials"):
         # Figure and Axes objects to contain our plots
         self._fig = plt.figure()
@@ -24,7 +32,7 @@ class PelvicGraph(object):
         
         plt.title(name)
 
-def default_color(fiducial): 
+def default_color_fn(fiducial): 
     ''' Default coloration function. Clone this and alter it to customize your coloration when graphing. '''
     return DEFAULT_COLOR
 
@@ -32,15 +40,16 @@ def show_all_graphs():
     ''' Display the graph.  Call this after adding all scatterpoints to it. '''
     plt.show()
 
-def add_scatterpoint_to_graph(graph, name, x, y, z, newcolor="black"):
+def add_scatterpoint_to_graph3D(graph, name, x, y, z, newcolor="black"):
     ''' Add a new scatterpoint to the graph.  Name is currently ignored. '''
     graph._ax.scatter(x,y,z, marker='o', label=name, color=newcolor)
 
-def add_line_to_graph(graph, pt1,pt2, newcolor):
+def add_line_to_graph3D(graph, pt1,pt2, newcolor):
     ''' Draws a line from [x1,y1,z1] to [x2,y2,z2] of color newcolor.'''
     graph._ax.plot([pt1[0],pt2[0]], [pt1[1],pt2[1]], [pt1[2],pt2[2]], color=newcolor)
 
-def add_fiducials_to_graph(graph, vagprops, color_fn = default_color):
+
+def add_fiducials_to_graph3D(graph, vagprops, color_fn = default_color_fn):
     
     fid_list = vagprops._fiducial_points
     
@@ -49,17 +58,9 @@ def add_fiducials_to_graph(graph, vagprops, color_fn = default_color):
         
         fid = fid_list[key]
         debugprint("Adding fiducial to graph: " + fid.to_string(), debug_levels.DETAILED_DEBUG)
-        add_scatterpoint_to_graph(graph, fid.name, fid.coords[COORDS.X], fid.coords[COORDS.Y], fid.coords[COORDS.Z], color_fn(fid, vagprops))
+        add_scatterpoint_to_graph3D(graph, fid.name, fid.coords[COORDS.X], fid.coords[COORDS.Y], fid.coords[COORDS.Z], color_fn(fid, vagprops))
         
-## BELOW FN IS CURRRENTLY UNUSED.
-def add_columns_to_graph(graph, fid_list, color_fn = default_color):
-    ''' Add all fiducials in the dictionary fid_list to the graph as columns from Z=-50.  Color code using the function color_fn which takes a fiducial as an argument. '''
-    
-    for key in fid_list.iterkeys():
-        fid = fid_list[key]
-        add_line_to_graph(graph, fid.coords, [fid.coords[COORDS.X], fid.coords[COORDS.Y], COLUMN_START], color_fn(fid))
-        
-def add_legend_to_graph(graph, minlabel, maxlabel, mincolor, maxcolor):
+def add_legend_to_graph3D(graph, minlabel, maxlabel, mincolor, maxcolor):
     ''' Create a legend for the graph, using matched tuples of labels and colors (e.g. labels[0] gets colors[0]'''
        
     # Create fake lines to fool the legend.    
@@ -68,7 +69,8 @@ def add_legend_to_graph(graph, minlabel, maxlabel, mincolor, maxcolor):
     
     graph._ax.legend([minline,maxline], [minlabel,maxlabel], numpoints=2, loc="lower right")
     
-def add_ellipsoid_to_graph(graph, center_coords, x_diam, y_diam, z_diam): 
+# BELOW FUNCTION CURRENTLY UNUSED
+def add_ellipsoid_to_graph3D(graph, center_coords, x_diam, y_diam, z_diam): 
     ''' Ellipsoid code from http://matplotlib.org/examples/mplot3d/surface3d_demo2.html'''
     
     u = np.linspace(0, 2 * np.pi, 100)
@@ -78,23 +80,22 @@ def add_ellipsoid_to_graph(graph, center_coords, x_diam, y_diam, z_diam):
     y = center_coords[COORDS.Y] + y_diam * np.outer(np.sin(u), np.sin(v))
     z = center_coords[COORDS.Z] + z_diam * np.outer(np.ones(np.size(u)), np.cos(v))
     graph._ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b')
-     
-    
-def set_graph_boundaries(graph, min_x, max_x, min_y, max_y, min_z, max_z):
+        
+def set_graph_boundaries3D(graph, min_x, max_x, min_y, max_y, min_z, max_z):
     
     # HACK HACK HACK
     # Because 3D axis boundary setting doesn't work well yet, we'll just draw a scatterpoint in black at each corner to force the issue.
-    add_scatterpoint_to_graph(graph,"FakePoint", min_x, min_y, min_z)
-    add_scatterpoint_to_graph(graph,"FakePoint", min_x, max_y, min_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", min_x, min_y, min_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", min_x, max_y, min_z)
     
-    add_scatterpoint_to_graph(graph,"FakePoint", min_x, min_y, max_z)
-    add_scatterpoint_to_graph(graph,"FakePoint", min_x, max_y, max_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", min_x, min_y, max_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", min_x, max_y, max_z)
 
-    add_scatterpoint_to_graph(graph,"FakePoint", max_x, min_y, min_z)
-    add_scatterpoint_to_graph(graph,"FakePoint", max_x, max_y, min_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", max_x, min_y, min_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", max_x, max_y, min_z)
     
-    add_scatterpoint_to_graph(graph,"FakePoint", max_x, min_y, max_z)
-    add_scatterpoint_to_graph(graph,"FakePoint", max_x, max_y, max_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", max_x, min_y, max_z)
+    add_scatterpoint_to_graph3D(graph,"FakePoint", max_x, max_y, max_z)
 
 
     
