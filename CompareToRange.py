88f568eafa3ab456f3251332371a9c17117043bf
Sean Lisse <sean.lisse@gmail.com>
2013-03-10 21:53:38 -0400

    Add the ability to plot paravaginal gap in a separate graph.

--- CompareToRange.py
+++ CompareToRange.py
@@ -22,20 +22,92 @@ BAR_COLOR = "#EEEEEE"
 POINT_COLOR = "#000000"
 COORD_TO_GRAPH=COORDS.Z
 
+GRAPH_MAX_MM = 100
+GRAPH_MIN_MM = -50
+
+PARAVAG_MAX_MM = 100
+PARAVAG_MIN_MM = -10
+
+def create_2D_paravaginal_graph(graph, graphname, exemplardisplay, rangestats):
+    ''' TODO: DOCUMENT ME '''
+    
+    if (graph == None):
+        debugprint("No graph given! ",debug_levels.ERROR)
+        return
+    
+    fid_list = exemplardisplay._fiducial_points
+    stat_list = rangestats.get_all_stats()
+    
+    fid_length = len(fid_list)
+    graph.set_xlim(0,fid_length)
+    graph.set_ylim(PARAVAG_MIN_MM, PARAVAG_MAX_MM)
+    
+    # Start with a blank point at 0 index so our first datapoint isn't right on the edge of the graph.
+    x_index = 0
+    x_labels = [""] 
+    
+    for key in fid_list.iterkeys():
+        
+        x_index += 1
+        x_labels.append(key)
+
+        # Draw the "error bar" on the graph in light grey
+        if (key in stat_list):
+            stat = stat_list[key]
+        else: 
+            stat = None
+              
+        if (stat != None): 
+            avg_val = stat._averaged_paravag_gap
+            
+            stdev = stat._fid_paravag_gap_std_dev
+            
+            min_val = avg_val - (STD_DEV_GRAPH_MULTIPLIER * stdev)
+            max_val = avg_val + (STD_DEV_GRAPH_MULTIPLIER * stdev)
+            
+            if (key in REFERENCE_POINT_NAMES):
+                barcolor = REFERENCE_POINT_BAR_COLOR
+            else: 
+                barcolor = BAR_COLOR
+
+            # Draw the range
+            graph.broken_barh([(x_index-0.5,1)], (min_val, max_val - min_val), facecolors=barcolor)
+            
+            # Draw the average val
+            graph.broken_barh([(x_index-0.5,1)], (avg_val, 0), facecolors=barcolor)
+            
+            # print("Stats for " + key + ": avg " + str(avg_val) + ", stdev " + str(stdev) + ", min " + str(min_val) + ", max " + str(max_val))
+        
+        # Draw the fiducial as a black dot.
+        fid = fid_list[key]
+        graph.scatter(x_index, fid.paravaginal_gap, marker='o', label=key, color=POINT_COLOR)
+        
+    # Add an extra space on the end so our last point isn't on the edge of the graph.
+    x_index += 2
+    x_labels.append("")
+    x_labels.append("")
+        
+    graph.set_xticks(range(0, x_index))    
+    label_obj = graph.set_xticklabels(x_labels)       
+
+    plt.setp(label_obj, rotation=45, fontsize=8)
+
+    graph.grid(True)
+
+    return graph   
 
 def create_2D_comparison_graph(graph, graphname, exemplardisplay, rangestats):
-
-    if (graph == None):
-        graph = PelvicGraph2D(graphname)
-
-    [color_fn, minmax_distances] = calibrate_colorization_strategy_fn(exemplardisplay)
-        
     ''' Add all fiducials in the RANGE dictionary fid_list to the graph as bars.  Color code using the function color_fn which takes a fiducial as an argument. '''
+        
+    if (graph == None):
+        debugprint("No graph given! ",debug_levels.ERROR)
+        return
+    
     fid_list = exemplardisplay._fiducial_points
     
     fid_length = len(fid_list)
-    graph._ax.set_xlim(0,fid_length)
-    graph._ax.set_ylim(-50,100)
+    graph.set_xlim(0,fid_length)
+    graph.set_ylim(GRAPH_MIN_MM, GRAPH_MAX_MM)
     
     stat_list = rangestats.get_all_stats()
     
@@ -71,34 +143,34 @@ def create_2D_comparison_graph(graph, graphname, exemplardisplay, rangestats):
                 barcolor = BAR_COLOR
 
             # Draw the range
-            graph._ax.broken_barh([(x_index-0.5,1)], (min_val, max_val - min_val), facecolors=barcolor)
+            graph.broken_barh([(x_index-0.5,1)], (min_val, max_val - min_val), facecolors=barcolor)
             
             # Draw the average val
-            graph._ax.broken_barh([(x_index-0.5,1)], (avg_val, 0), facecolors=barcolor)
+            graph.broken_barh([(x_index-0.5,1)], (avg_val, 0), facecolors=barcolor)
             
             # print("Stats for " + key + ": avg " + str(avg_val) + ", stdev " + str(stdev) + ", min " + str(min_val) + ", max " + str(max_val))
         
         # Draw the fiducial as a black dot.
         fid = fid_list[key]
-        graph._ax.scatter(x_index, fid.coords[COORD_TO_GRAPH], marker='o', label=key, color=POINT_COLOR)
+        graph.scatter(x_index, fid.coords[COORD_TO_GRAPH], marker='o', label=key, color=POINT_COLOR)
     
     # Add an extra space on the end so our last point isn't on the edge of the graph.
     x_index += 2
     x_labels.append("")
     x_labels.append("")
         
-    graph._ax.set_xticks(range(0, x_index))    
-    label_obj = graph._ax.set_xticklabels(x_labels)       
+    graph.set_xticks(range(0, x_index))    
+    label_obj = graph.set_xticklabels(x_labels)       
 
     plt.setp(label_obj, rotation=45, fontsize=8)
 
-    graph._ax.grid(True)
+    graph.grid(True)
 
     return graph   
 
 #####################
 ### DEFAULT MAIN PROC
-#####################  
+#####################  graph
 
 if __name__ == '__main__':
         
@@ -119,6 +191,14 @@ if __name__ == '__main__':
         rangelist = load_vaginal_properties(argv[2:])
         [rangestats, rangedisplay] = get_stats_and_display_from_properties("Range", rangelist)
      
-        graph = create_2D_comparison_graph(None, argv[1] + " Range Comparison", propsdisplay, rangestats)
+        fig = plt.figure()
+        
+        graph = fig.add_subplot(211)
+        
+        create_2D_comparison_graph(graph, argv[1] + " Range Comparison", propsdisplay, rangestats)
+        
+        graph2 = fig.add_subplot(212)
+        
+        create_2D_paravaginal_graph(graph2, argv[1] + " Paravaginal Comparison", propsdisplay, rangestats)
         
     show_all_graphs()
