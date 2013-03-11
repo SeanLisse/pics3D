838ccc44faf1427de62e7176fb00b83c5e053e4e
Sean Lisse <sean.lisse@gmail.com>
2013-03-10 21:53:01 -0400

    Refactor 'fiducial' into 'Fiducial' to make it clearer that it's a class.  Also isolate VaginalDisplay to its own file to eliminate circular dependencies.

---
+++ VaginalDisplay.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# Author: Sean Lisse
+# Define a class to encapsulate a vagina and its display method
+
+from VaginalProperties import VaginalProperties
+
+from GraphColoring import DEFAULT_COLORIZATION_STRATEGY
+
+class VaginalDisplay(VaginalProperties):
+ 
+    # What strategy should we use to display this vagina?
+    _color_strategy = None
+    
+    # Vaginal width list (indexed by fiducial rows)
+    _vagrowcolors = None
+    
+    def __init__(self, name, color_strat = DEFAULT_COLORIZATION_STRATEGY ):
+        VaginalProperties.__init__(self, name)
+        
+        self._color_strategy = color_strat
+        
+        self._vagrowcolors=[]
